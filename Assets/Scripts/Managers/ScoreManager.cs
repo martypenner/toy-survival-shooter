fc1b6421a4c58296c3030c7ec41852365b1fb914
Marty Penner <marty@gotenzing.com>
2014-10-28 18:32:14 -0400

    Initial commit w/ project tutorial completed

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+
+    Text scoreText;
+
+    void Awake ()
+    {
+        scoreText = GetComponent <Text> ();
+        score = 0;
+    }
+
+    void Update ()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
